960fef7c31a9c62dfa4c2a9c4c59146e159cf9e5
AmanSingh-tech <85844792+AmanSingh-tech@users.noreply.github.com>
2024-10-15 16:06:02 +0530

    Add files via upload

---
+++ src/app/Error.tsx
@@ -0,0 +1,9 @@
+"user cleint"
+export default function Error() {
+    return (
+        <main className="m-auto my-10 max-w-5x1 space-y-5 px-3 text-center">
+            <h1>Error</h1>
+            <p>An unexpected error occurred</p>
+        </main>
+    )
+}
